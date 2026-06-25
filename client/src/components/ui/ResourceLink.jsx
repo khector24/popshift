a1fb69f1d461ddab0def2d4b6f221dbe583a87ce
Kenny Hector <kennyhector24@gmail.com>
2026-06-25 13:33:52 -0400

    Support multiple resource links

--- client/src/components/ui/ResourceLink.jsx
+++ client/src/components/ui/ResourceLink.jsx
@@ -1,17 +1,36 @@
 import "../../styles/components/ResourceLink.css";
-export default function ResourceLink({ label, text, url }) {
+
+export default function ResourceLink({ label, text, url, links = [] }) {
+  const hasLinks = links.length > 0;
+
   return (
     <p className="resource-link">
       <span className="resource-link__label">{label}: </span>
 
-      <a
-        className="resource-link__anchor"
-        href={url}
-        target="_blank"
-        rel="noreferrer"
-      >
-        {text}
-      </a>
+      {hasLinks ? (
+        links.map((link, index) => (
+          <span key={link.url}>
+            <a
+              className="resource-link__anchor"
+              href={link.url}
+              target="_blank"
+              rel="noreferrer"
+            >
+              {link.text}
+            </a>
+            {index < links.length - 1 && ", "}
+          </span>
+        ))
+      ) : (
+        <a
+          className="resource-link__anchor"
+          href={url}
+          target="_blank"
+          rel="noreferrer"
+        >
+          {text}
+        </a>
+      )}
     </p>
   );
 }
