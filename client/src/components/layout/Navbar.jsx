4ab9b854cdd958667b250b6c220cb72a9fd7d28b
Kenny Hector <kennyhector24@gmail.com>
2026-07-14 22:20:45 -0400

    Add responsive navbar with mobile dropdown menu

--- client/src/components/layout/Navbar.jsx
+++ client/src/components/layout/Navbar.jsx
@@ -1,25 +1,62 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
+
 import GlobalSearch from "./GlobalSearch";
+
 import "../../styles/components/Navbar.css";
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  function closeMenu() {
+    setMenuOpen(false);
+  }
+
   return (
     <nav className="navbar">
       <div className="navbar__content">
-        <NavLink className="navbar__logo" to="/">
+        <NavLink className="navbar__logo" to="/" onClick={closeMenu}>
           PopShift
         </NavLink>
 
-        <GlobalSearch />
+        <button
+          className={`navbar__menu-button ${
+            menuOpen ? "navbar__menu-button--open" : ""
+          }`}
+          type="button"
+          aria-label="Toggle navigation"
+          aria-expanded={menuOpen}
+          onClick={() => setMenuOpen((currentValue) => !currentValue)}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
 
-        <div className="navbar__links">
-          <NavLink to="/states">States</NavLink>
-          <NavLink to="/metros">Metros</NavLink>
-          {/* Future */}
-          {/* <NavLink to="/explore-moving">Explore Moving</NavLink>
-          <NavLink to="/articles">Articles</NavLink> */}
-          <NavLink to="/about">About</NavLink>
-          <NavLink to="/dashboard">dashboard</NavLink>
+        <div
+          className={`navbar__panel ${menuOpen ? "navbar__panel--open" : ""}`}
+        >
+          <div className="navbar__search">
+            <GlobalSearch />
+          </div>
+
+          <div className="navbar__links">
+            <NavLink to="/states" onClick={closeMenu}>
+              States
+            </NavLink>
+
+            <NavLink to="/metros" onClick={closeMenu}>
+              Metros
+            </NavLink>
+
+            <NavLink to="/about" onClick={closeMenu}>
+              About
+            </NavLink>
+
+            <NavLink to="/dashboard" onClick={closeMenu}>
+              Dashboard
+            </NavLink>
+          </div>
         </div>
       </div>
     </nav>
